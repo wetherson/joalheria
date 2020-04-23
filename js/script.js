ebf967c95649a2a9bd9c27390df3da325baa06a9
wetherson <wetherson@gmail.com>
2020-04-23 04:56:27 -0300

    Slideshow com click Proximo e Anterior
    Slideshow com click Próximo e Anterior funcionando.
    Trabalhar agora com os botões inferiores.

--- js/script.js
+++ js/script.js
@@ -1,50 +1,66 @@
-var slideIndex = 0;
+var slideIndex  = 0;
+var maximoSlide = 3;
 var segundos = 5; /*Segundos */ 
 slideAutomatico();
 
+/* Função que faz as fotos girarem automáticamente */
 function slideAutomatico() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var botoes = document.getElementsByClassName("botao");
-  for (i = 0; i < slides.length; i++) {
+
+// Esconde todos os slides e todos os Botoes
+  for (i = 0; i < maximoSlide; i++) {
     slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) 
-    {slideIndex = 1}    
-  for (i = 0; i < botoes.length; i++) {
     botoes[i].className = botoes[i].className.replace(" active", "");
   }
+  slideIndex++;
+  if (slideIndex > maximoSlide) {
+    slideIndex = 1;
+  }    
   slides[slideIndex-1].style.display = "block";  
   botoes[slideIndex-1].className += " active";
   setTimeout(slideAutomatico, segundos * 1000 ); 
 }
 
+/* Função que vai para uma foto diretamente quando se clica nos  */
+/* Botões inferiores em formato de circulo                       */
 function mostraSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var botoes = document.getElementsByClassName("botao");
-  if (n > slides.length) 
-    {slideIndex = 1}
-  if (n < 1) 
-    {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  slideIndex = n; 
+  // Esconde todos os slides e todos os Botoes
+  for (i = 0; i < maximoSlide; i++) {
       slides[i].style.display = "none";
+      botoes[i].className = botoes[i].className.replace(" active", "");
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex -1].style.display = "block";  
+  botoes[slideIndex -1].className += " active";
 }
 
-// Controle de Proximo e Anterior
-function aumentaSlides(n) {
-    mostraSlides(slideIndex += n);
+// Controle Setas de Proximo e Anterior
+// X somente pode ser A (Acrescenta) ou D (Dimunui)
+function aumentaSlides(X) {
+//  alert('X ' + X  + ' slideIndex ' + slideIndex + ' maximoSlide ' + maximoSlide )
+  if ( X == 'A' ) {
+    if (slideIndex == maximoSlide)
+      slideIndex = 1;
+    else {
+      slideIndex++;
+    }
+  } else {  // X = D
+   if (slideIndex == 1) {
+      slideIndex = maximoSlide;
+    }else {
+      slideIndex--;
+    }
   }
+//  alert('slideIndex ' + slideIndex)
+  mostraSlides(slideIndex);
+}
   
   // Thumbnail image controls
-  function vaiSlide(n) {
-    mostraSlides(slideIndex = n);
-  }
-  
+  //function vaiSlide(n) {
+//    mostraSlides(slideIndex = n);
+//  }  
